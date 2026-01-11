d1a3fca26d80b936b9124448f52140cf4cba2f97
tapan0810 <arceus0810@gmail.com>
2026-01-11 10:48:56 +0530

    Add EmployeeAdminPortal API

---
+++ EmployeeAdminPortal.API-master/EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,97 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = dbContext.Employees.ToList();
+
+            return Ok(allEmployees);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+            };
+
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+
+            return Ok(employeeEntity);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id, UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = updateEmployeeDto.Name;
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+
+
+            dbContext.SaveChanges();
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteEmployeeById(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee == null)
+                {
+                return NotFound();
+            }
+            dbContext.Employees.Remove(employee);
+            dbContext.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+}
---
+++ EmployeeAdminPortal.API-master/EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<EmployeeAdminPortal.Models.Entities.Employee> Employees { get; set; }
+    }
+}
